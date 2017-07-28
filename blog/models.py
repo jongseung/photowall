77975eb97534e48b2ba88e454766224bc1fe0379
jongseung <park930902@gmail.com>
2017-07-28 15:54:01 +0900

    blog 앱 내 Post에 대한 댓글 쓰기/수정/삭제 구현

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 import re
 from django import forms
+from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.db import models
 
 def lnglat_validator(value):
@@ -17,6 +19,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
 
     @property
     def lng(self):
@@ -29,3 +34,21 @@ class Post(models.Model):
         if self.lnglat:
             return self.lnglat.split(',')[1]
         return None
+
+class Comment(models.Model):
+    # Post : comment = 1 : N
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+    #댓글 최그 순서
+    class Meta:
+        ordering = ['-id']
+    
+    def get_edit_url(self):
+        return reverse('blog:comment_edit', args = [self.post.pk, self.pk])
+    
+    def get_delete_url(self):
+        return reverse('blog:comment_delete', args = [self.post.pk, self.pk])
